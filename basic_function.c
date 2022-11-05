0b60010eafec6c5de93c031af3ee2e5eb8dfb9e9
sprathamesh <89856720+sprathamesh@users.noreply.github.com>
2022-11-06 01:32:36 +0530

    Add files via upload

---
+++ basic_function.c
@@ -0,0 +1,28 @@
+//use of function 
+#include<stdio.h>
+void GoodMorning();
+void GoodAfternoon();
+void GoodNight();
+
+int main()
+{
+	GoodMorning();
+    GoodAfternoon();
+    GoodNight();
+   return 0;	
+}	
+
+void GoodMorning()
+{
+	printf("GOOD MORNING \n");
+}
+
+void GoodAfternoon()
+{
+	printf("GOOD AFTERNOON\n");
+}
+
+void GoodNight()
+{
+	printf("GOOD NIGHT\n");
+}
